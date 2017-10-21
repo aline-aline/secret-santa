bfbb4df757e6c91196d164b503f713cad1a8c1bc
aline-aline <alinecris@gmail.com>
2017-10-21 16:33:26 -0200

    Create service Raffle and set devise in rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,10 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
+RSpec.configure do |config|
+  config.include Devise::Test::ControllerHelpers, type: :controller
+end
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
