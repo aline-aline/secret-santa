7c1f201f51b143971ff16ad13648b2be707bf520
aline-aline <alinecris@gmail.com>
2017-11-07 22:02:18 -0200

    Setup nginx

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  config.action_mailer.default_url_options = { :host => 'http://localhost:3000' }
+  #config.action_mailer.default_url_options = { :host => 'http://localhost:3000' }
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.raise_delivery_errors = true
