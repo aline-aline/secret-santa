4e0b38cf97ad837376799a01d4357080f945a2f6
aline-aline <alinecris@gmail.com>
2017-11-09 23:07:51 -0200

    Add test to the job raffle

--- spec/jobs/campaign_raffle_job_spec.rb
+++ spec/jobs/campaign_raffle_job_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe CampaignRaffleJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  include ActiveJob::TestHelper
+
+  before(:each) do
+    @current_user = FactoryGirl.create(:user)
+    @campaign = create(:campaign, user: @current_user)
+    @job = CampaignRaffleJob.perform_later(@campaign)
+  end
+
+  it 'queues the job' do
+    expect { CampaignRaffleJob.perform_later(@campaign) }
+      .to have_enqueued_job(CampaignRaffleJob)
+  end
+
+  it 'is in emails queue' do
+    expect(CampaignRaffleJob.new.queue_name).to eq('emails')
+  end
+
+  it 'executes perform' do
+    expect(RaffleService.new(@campaign))
+    perform_enqueued_jobs { @job }
+  end
 end
+
+
